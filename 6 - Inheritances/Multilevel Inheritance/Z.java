f6700217938f44fa3da8520c7dfce5c7078ab632
Raghu <m.raghavendra331@gmail.com>
2025-06-10 22:39:14 +0530

    Updated to 7

--- 6 - Inheritances/Multilevel Inheritance/Z.java
+++ 6 - Inheritances/Multilevel Inheritance/Z.java
@@ -17,9 +17,9 @@ class Z extends Y {
 
     public static void main(String args[]) {
         Z obj = new Z();
-        obj.methodX(); // calling grand parent class method
-        obj.methodY(); // calling parent class method
-        obj.methodZ(); // calling local method
+        obj.methodX(); 
+        obj.methodY(); 
+        obj.methodZ(); 
     }
 }
 
--- 6 - Inheritances/Single Inheritance/B.java
+++ 6 - Inheritances/Single Inheritance/B.java
@@ -1,23 +1,23 @@
 class A
 {
-   public void methodA()
-   {
-     System.out.println("Base class method");
-   }
+  public void methodA()
+  {
+    System.out.println("Base class method");
+  }
 }
 
 class B extends A
 {
-   public void methodB()
-   {
-     System.out.println("Child class method");
-   }
-   public static void main(String args[])
-   {
-     B obj = new B();
-     obj.methodA(); 
-     obj.methodB(); 
+  public void methodB()
+  {
+    System.out.println("Child class method");
   }
+  public static void main(String args[])
+  {
+    B obj = new B();
+    obj.methodA(); 
+    obj.methodB(); 
+}
 }
 
 /**
